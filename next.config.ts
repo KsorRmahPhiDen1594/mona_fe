9a87e227904b2e0626d86273db12640061e398c5
KsorRmahPhiDen1594 <phiden1594@gmail.com>
2025-07-03 16:09:46 +0700

    basic build website

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,41 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const createNextIntlPlugin = require('next-intl/plugin');
+const withNextIntl = createNextIntlPlugin();
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    siteKey: process.env.SITE_KEY,
+    copySecretKey: process.env.COPY_SECRET_KEY,
+    resendKey: process.env.RESEND_KEY,
+    IPINFO_TOKEN: process.env.IPINFO_TOKEN,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
+        pathname: "/f/**",
+      },
+      {
+        protocol: "https",
+        hostname: "in8cddcab4.ufs.sh",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "github.com",
+        port: "",
+        pathname: "/f/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = withNextIntl(nextConfig);
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { NavigationMenuDemo } from "./example-navigation-menu";
+
+const DashboardPage = () => {
+  return (
+    <div className="h-screen container section">
+      <NavigationMenuDemo />
+    </div>
+  );
+};
+
+export default DashboardPage;
