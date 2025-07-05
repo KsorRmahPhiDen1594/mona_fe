c5e5ffff2c7171b756cf0ef774f614f0ca9330c5
Trần Trân Lộc <tran26122003@gmail.com>
2025-07-05 11:40:07 +0700

    feat: add header with navigation, search functionality and colored hover effects

---
+++ src/components/ui/brand-logo.tsx
@@ -0,0 +1,16 @@
+import { cn } from "@/lib/utils";
+
+const BrandLogo = ({ dark = false, className }: { dark?: boolean; className?: string }) => {
+  const logoSrc = dark ? "https://mona.media/wp-content/uploads/2024/10/media-logo-new-1.png" : "https://mona.media/wp-content/uploads/2024/10/media-logo-new-1.png";
+  return (
+    <div className={cn(className, "flex items-center gap-2")}>
+      <img
+        src={logoSrc}
+        alt="Brand Logo"
+        className=" w-[380px] object-contain"
+      />
+    </div>
+  );
+};
+
+export default BrandLogo; 
